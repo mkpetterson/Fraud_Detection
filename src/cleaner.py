10cd12842523bc35e1768afd81d9a18c01701c42
Wesley Nguyen <nguyenw@seattleu.edu>
2020-05-28 13:58:44 -0700

    aggregate cleaner

--- src/cleaner.py
+++ src/cleaner.py
@@ -10,23 +10,25 @@ import numpy as np
 
 
 keep = ['country', 'currency', 'delivery_method', 'email_domain', 'event_start', 
-                    'fb_published', 'listed', 'object_id', 'payee_name', 'payout_type', 
-                    'previous_payouts', 'user_age', 'user_type', 'venue_country', 'venue_latitude', 
-                    'venue_longitude', 'venue_name', 'venue_state']
+        'fb_published', 'listed', 'object_id', 'payee_name', 'payout_type', 
+        'previous_payouts', 'user_age', 'user_type', 'venue_country', 'venue_latitude', 
+        'venue_longitude', 'venue_name', 'venue_state']
 
-def clean_data(data:any) -> pd.DataFrame:
-    data = data[keep]
-    return data
+
+def clean_with_target(data:any) -> pd.DataFrame:
+    """ Returns clean dataframe with wanted columns """
+    
+    data_cp = data[keep + ['acct_type']].copy()
+    data_cp['fraud'] = data_cp['acct_type'].apply(lambda x: 0 if x == 'premium' else 1)
+    data_cp.drop(columns='acct_type', inplace=True)
+    
+    return data_cp
+
+
+
 
 
 def clean_row(call:any) -> pd.Series:
-    pass
-
-
-def create_target(df):
-    """ Take in dataframe and generate binary target"""
-
-    df['fraud'] = df['acct_type'].apply(lambda x: 0 if x == 'premium' else 1)
-    df.drop(columns='acct_type', inplace=True)
+    """ Returns clean series with wanted columns, intake taken from Client API call """
     
-    return df
+    pass
