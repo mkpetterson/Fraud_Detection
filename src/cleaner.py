95750f8d7851dd2f0a4c4a9697ddaa45d496ccb0
Wesley Nguyen <nguyenw@seattleu.edu>
2020-05-28 12:29:30 -0700

    start cleaner

---
+++ src/cleaner.py
@@ -0,0 +1,16 @@
+# Import Modules
+from pprint import pprint
+import json
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+import re
+
+import pandas as pd
+import numpy as np
+
+
+def clean_data(data:any) -> pd.DataFrame:
+    pass
+
+def clean_row(call:any) -> pd.Series:
+    pass
