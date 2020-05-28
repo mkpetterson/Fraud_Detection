0f04cec4e500d036d2e1d69c2378dd73f970ba69
Wesley Nguyen <nguyenw@seattleu.edu>
2020-05-28 12:39:42 -0700

    create cleaner

--- src/cleaner.py
+++ src/cleaner.py
@@ -9,8 +9,15 @@ import pandas as pd
 import numpy as np
 
 
+keep = ['country', 'currency', 'delivery_method', 'email_domain', 'event_start', 
+                    'fb_published', 'listed', 'object_id', 'payee_name', 'payout_type', 
+                    'previous_payouts', 'user_age', 'user_type', 'venue_country', 'venue_latitude', 
+                    'venue_longitude', 'venue_name', 'venue_state']
+
 def clean_data(data:any) -> pd.DataFrame:
-    pass
+    data = data[keep]
+    return data
+
 
 def clean_row(call:any) -> pd.Series:
     pass
