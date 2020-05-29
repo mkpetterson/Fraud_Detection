4227285aedde503285d6c9c0f91bfc2744ba05af
mkpetterson <maureen.petterson@gmail.com>
2020-05-28 17:49:10 -0700

    added pickle

--- src/model.py
+++ src/model.py
@@ -8,7 +8,6 @@ from src import cleaner
 from sklearn.ensemble import  RandomForestClassifier
 
 
-
 def make_model(df):
     """Fit and pickle model"""
     
@@ -30,27 +29,9 @@ def make_model(df):
         
     return None
     
-    
 
 def nuclear_option(new_data):
-    """Takes in the cleaned dataframe and outputs a super simple df for initial model"""
-    
-#     # Eviscerate dataset
-#     to_keep = ['channels', 'fb_published', 'has_logo', 'user_type', 'fraud', 'n_previous_payouts']
-#     model_df = df[to_keep]
-#     model_df = model_df.dropna()
-    
-#     # Make model
-#     y = model_df.pop('fraud')
-#     X = model_df.copy()
-    
-#     model = RandomForestClassifier()
-#     model.fit(X,y)
-    
-    
-#     with open("model_name.pkl", 'w') as f:
-#         pickle.dump(model, f)
-    
+    """Takes in the model and new data and predicts fraud"""
     
     with open('../models/random_forest_model.pkl', 'rb') as f:
         model = pickle.load(f)
