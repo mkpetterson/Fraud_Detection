0548dbf4b5ae54080ea141ea0a7a0f7be179680c
mkpetterson <maureen.petterson@gmail.com>
2020-05-28 17:45:28 -0700

    updating

--- src/cleaner.py
+++ src/cleaner.py
@@ -33,7 +33,7 @@ def clean_with_target(data:any) -> pd.DataFrame:
 
 def clean_row(call:any) -> pd.Series:
     """ Returns clean series with wanted columns, intake taken from Client API call """
-    to_keep = ['channels', 'fb_published', 'has_logo', 'user_type', 'fraud', 'n_previous_payouts']
+    to_keep = ['channels', 'fb_published', 'has_logo', 'user_type', 'n_previous_payouts']
     
     # Put into dataframe
     data_cp = pd.DataFrame(call)
--- src/model.py
+++ src/model.py
@@ -32,26 +32,29 @@ def make_model(df):
     
     
 
-def nuclear_option(df, new_data):
+def nuclear_option(new_data):
     """Takes in the cleaned dataframe and outputs a super simple df for initial model"""
     
-    # Eviscerate dataset
-    to_keep = ['channels', 'fb_published', 'has_logo', 'user_type', 'fraud', 'n_previous_payouts']
-    model_df = df[to_keep]
-    model_df = model_df.dropna()
+#     # Eviscerate dataset
+#     to_keep = ['channels', 'fb_published', 'has_logo', 'user_type', 'fraud', 'n_previous_payouts']
+#     model_df = df[to_keep]
+#     model_df = model_df.dropna()
     
-    # Make model
-    y = model_df.pop('fraud')
-    X = model_df.copy()
+#     # Make model
+#     y = model_df.pop('fraud')
+#     X = model_df.copy()
     
-    model = RandomForestClassifier()
-    model.fit(X,y)
-    
-    
-    with open("model_name.pkl", 'w') as f:
-        pickle.dump(model, f)
+#     model = RandomForestClassifier()
+#     model.fit(X,y)
     
     
+#     with open("model_name.pkl", 'w') as f:
+#         pickle.dump(model, f)
+    
+    
+    with open('../models/random_forest_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
     # Predict on new data. Proba function returns prob of class [0,1]
     new_data = np.array(cleaner.clean_row(new_data))
     y_hat = model.predict(new_data)
