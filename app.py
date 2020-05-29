615617b4d573eadc53ff6cfe885f52ff19c0ca80
Ubuntu <ubuntu@ip-172-31-20-97.us-west-2.compute.internal>
2020-05-29 04:52:33 +0000

    add app

---
+++ app.py
@@ -0,0 +1,67 @@
+#Imports
+from flask import Flask, request
+app = Flask(__name__)
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+#from src import pred
+#from src import clean_data_func
+from src import api_client
+import pymongo
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.fraud_detection
+
+#Setup
+client = api_client.EventAPIClient()
+
+#Routes
+@app.route('/', methods=['GET'])
+def home():
+    return ''' 
+    <h1>Project Title</h1>
+    <p><a href="/dashboard">dashboard</a></p>
+    <p> nothing here, friend, but a link to 
+                   <a href="/hello">hello</a> and an 
+                   <a href="/form_example">example form</a> </p> 
+                   '''
+
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return ''' <h1> Hello, World!</h1> '''
+
+@app.route('/form_example', methods=['GET'])
+def form_display():
+    return ''' <form action="/string_reverse" method="POST">
+                <input type="text" name="some_string" />
+                <input type="submit" />
+               </form>
+             '''
+
+@app.route('/string_reverse', methods=['POST'])
+def reverse_string():
+    text = str(request.form['some_string'])
+    reversed_string = text[-1::-1]
+    return ''' output: {}  '''.format(reversed_string)
+
+    
+@app.route('/dashboard', methods=['GET'])
+def display_dash():
+    data = db.fraud_detection.find()
+    count = Counter()
+    for i in data:
+        count[i['data']['country']] += 1
+    fig, ax = plt.subplots()
+    x = np.arange(len(count.keys()))
+    y = count.values()
+    ax.bar(x, y)
+    fig.savefig('static/current.png')
+    return '''
+     <h1>Project Title</h1>
+     
+    <img src='static/current.png'>'''
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
